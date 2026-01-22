94157fe85183215f922330e8650d654ca940804a
JLucasGaldino <lucasgaldino@tuta.io>
2026-01-22 19:45:56 +0100

    part 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: 'https://JLucasGaldino.github.io',
+  base: '/demo-1',
   vite: {
     plugins: [
       tailwindcss()
