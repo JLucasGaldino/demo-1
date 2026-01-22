1a536e9e4dc4cdf6c3cd435141792cb7dbb4b2c7
JLucasGaldino <lucasgaldino@tuta.io>
2026-01-22 20:08:13 +0100

    Add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   site: 'https://JLucasGaldino.github.io',
   base: '/demo-1',
+
   vite: {
     plugins: [
       tailwindcss()
     ],
   },
+
+  adapter: netlify(),
 });
